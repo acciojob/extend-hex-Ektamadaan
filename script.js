9b1e02cf5278a41a45d7d4ed6e26f5ad224ec8df
Ektamadaan <ektamadaan45@gmail.com>
2023-12-15 13:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const r = shortHex[1];
+	const g = shortHex[2];
+	const b = shortHex[3];
+
+	const fullHex = `#${r}${r}${g}${g}${b}${b}` ;
+	return fullHex ;
 };
 
 // Do not change the code below.
